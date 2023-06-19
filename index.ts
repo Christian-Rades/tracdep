c3f8e61b20d39d81c177416fd9f505cde0d2397e
Christian Rades <christian.rades@googlemail.com>
2023-06-19 21:43:34 +0200

    feat: refactor input handling

--- index.ts
+++ index.ts
@@ -3,75 +3,33 @@ import Sigma from "sigma";
 import { Coordinates, EdgeDisplayData, NodeDisplayData } from "sigma/types"; 
 import forceAtlas2 from 'graphology-layout-forceatlas2';
 import FA2Layout from 'graphology-layout-forceatlas2/worker';
-import toSimple from 'graphology-operators/to-simple';
 import * as colorMap from './colorMap';
 
+import { newUnitGraph, UnitGraph } from "./input";
+import { parseLayersConfig, LayersConfig } from "./layers";
 
-let xmlHttp = new XMLHttpRequest();
-xmlHttp.open( "GET", "/dependencies.csv", false );
-xmlHttp.send( null );
-const data = xmlHttp.responseText;
+const unitGraph = newUnitGraph();
 
 
-xmlHttp = new XMLHttpRequest();
-xmlHttp.open( "GET", "/units.txt", false );
-xmlHttp.send( null );
+function createColorMap(graph: UnitGraph): Map<string, string> {
+    const packages = Array.from(graph.getPackages());
 
-const units: Set<string> = xmlHttp.responseText.split("\n")
-.slice(1)
-.filter(line => !/Test$/.test(line))
-.filter(line => !/\\Test\\/.test(line))
-.filter(line => !/SwagTest/.test(line))
-.filter(line => !/Shopware.Core.Framework.Log.Package/.test(line))
-.filter(line => !/Context$/.test(line))
-.reduce((set, name) => {
-    set.add(name);
-    return set;
-},
-    new Set<string>()
-);
+    const colors = colorMap.default({
+        colormap: 'jet',
+        nshades: packages.length,
+        format: 'hex',
+        alpha: 1
+    }).values();
 
-const packagesList = new Set<string>();
-const unitPackages= new Map<string, string>();
+    const packageColors=new  Map<string, string>();
 
-const edges = data.split("\n").
-reduce((edges: [string, string][], line) => {
-    const parts = line.split(",").map(p => p.trim());
-    if (parts.length < 2) {
-        return edges;
+    for (let pkg of packages) {
+        packageColors.set(pkg, colors.next().value);
     }
-
-    const self = parts[0];
-    if (!units.has(self)) {
-        return edges;
-    }
-
-    if (parts[1] != "") {
-        unitPackages.set(self, parts[1]);
-        packagesList.add(parts[1]);
-    }
-
-    for (const vertex of parts.slice(2)) {
-        if (!units.has(vertex) || vertex == self || vertex == "") {
-            continue;
-        }
-        edges.push([self, vertex]);
-    }
-
-    return edges;
-},[]);
-
-const colors = colorMap.default({
-    colormap: 'jet',
-    nshades: packagesList.size,
-    format: 'hex',
-    alpha: 1
-}).values();
-const packageColors=new  Map<string, string>();
-for (let pkg of packagesList) {
-    packageColors.set(pkg, colors.next().value);
+    return packageColors;
 }
 
+const packageColors = createColorMap(unitGraph);
 
 // Type and declare internal state:
 interface State {
@@ -81,30 +39,26 @@ interface State {
 
   // State derived from hovered node:
   hoveredNeighbors?: Set<string>;
+
+  //filter state
+  layersConfig?: LayersConfig;
+  
 }
 const state: State = {pinnend: false};
 
-
-
-
-const graph = new Graph();
-
 let i = 0;
-for (let unit of units) {
-
-    const pkg = unitPackages.get(unit)??'';
-    let color = packageColors.get(pkg) as string;
-
-    graph.addNode(unit, { x: Math.sin(i), y: Math.cos(i), size: 5, label: unit, color: color });
+unitGraph.graph.forEachNode((node: string, attributes) => {
+    attributes.x = Math.sin(i);
+    attributes.y = Math.cos(i);
+    attributes.size = 5;
+    attributes.label = node;
+    attributes.color = packageColors.get(attributes.package??'') as string;
     i++;
-}
+});
 
-for (const edge of edges) {
-    graph.addEdge(edge[0], edge[1]);
-}
 
-const sensibleSettings = forceAtlas2.inferSettings(graph);
-const layout = new FA2Layout(graph, {
+const sensibleSettings = forceAtlas2.inferSettings(unitGraph.graph);
+const layout = new FA2Layout(unitGraph.graph, {
   settings: sensibleSettings
 });
 
@@ -114,47 +68,96 @@ layout.start();
 // To stop the layout
 setTimeout(() => {
 layout.stop();
-}, 10000);
+}, 20000);
 
 
-const container = document.getElementById("sigma-container") as HTMLElement;
-const renderer = new Sigma(graph, container);
+function newRenderer(graph: Graph, state: State): Sigma {
+    const container = document.getElementById("sigma-container") as HTMLElement;
+    const renderer = new Sigma(graph, container);
 
 
-const output = document.getElementById("output") as HTMLTextAreaElement;
-renderer.on("clickNode", ({ node }) => {
-    state.pinnend = true;
-    setHoveredNode(node);
-    output.value = "";
-    graph.neighbors(node).forEach((n => {
-        output.value += n + "\n";
-    }));
-});
+    const output = document.getElementById("output") as HTMLTextAreaElement;
+    renderer.on("clickNode", ({ node }) => {
+        state.pinnend = true;
+        setHoveredNode(node);
+        output.value = "";
+        graph.neighbors(node).forEach((n => {
+            output.value += n + "\n";
+        }));
+    });
 
-function setHoveredNode(node?: string) {
-  if (node) {
-    state.hoveredNode = node;
-    state.hoveredNeighbors = new Set(graph.neighbors(node));
-  } else {
-    state.hoveredNode = undefined;
-    state.hoveredNeighbors = undefined;
-  }
+    function setHoveredNode(node?: string) {
+        if (node) {
+            state.hoveredNode = node;
+            state.hoveredNeighbors = new Set(graph.neighbors(node));
+        } else {
+            state.hoveredNode = undefined;
+            state.hoveredNeighbors = undefined;
+        }
 
-  // Refresh rendering:
-  renderer.refresh();
+        // Refresh rendering:
+        renderer.refresh();
+    }
+
+    // Bind graph interactions:
+    renderer.on("enterNode", ({ node }) => {
+        //setHoveredNode(node);
+    });
+    renderer.on("leaveNode", () => {
+        //setHoveredNode(undefined);
+    });
+    renderer.on("clickStage", () => {
+        state.pinnend = false;
+        setHoveredNode(undefined);
+    });
+
+
+    // Render nodes accordingly to the internal state:
+    // If there is a hovered node, all non-neighbor nodes are greyed
+    renderer.setSetting("nodeReducer", (node, data) => {
+        const res: Partial<NodeDisplayData> = { ...data };
+
+        if (state.hoveredNeighbors && !state.hoveredNeighbors.has(node) && state.hoveredNode !== node) {
+            res.label = "";
+            res.color = "#f6f6f6";
+            return res;
+        }
+
+        const pkg = graph.getNodeAttribute(node, 'package')??'';
+        if(!!state.selectedPackage) {
+            if (pkg != state.selectedPackage) {
+                res.label = "";
+                res.color = "#f6f6f6";
+            } else {
+                const color = packageColors.get(pkg) as string;
+                res.color = color;
+            }
+        }else {
+            const color = packageColors.get(pkg) as string;
+            res.color = color;
+        }
+
+
+        return res;
+    });
+
+    // Render edges accordingly to the internal state:
+    // 1. If a node is hovered, the edge is hidden if it is not connected to the
+    //    node
+    renderer.setSetting("edgeReducer", (edge, data) => {
+        const res: Partial<EdgeDisplayData> = { ...data };
+
+        if (state.hoveredNode && !graph.hasExtremity(edge, state.hoveredNode)) {
+            res.hidden = true;
+        }
+
+
+        return res;
+    });
+    return renderer;
 }
 
-// Bind graph interactions:
-renderer.on("enterNode", ({ node }) => {
-  //setHoveredNode(node);
-});
-renderer.on("leaveNode", () => {
-  //setHoveredNode(undefined);
-});
-renderer.on("clickStage", () => {
-  state.pinnend = false;
-  setHoveredNode(undefined);
-});
+const renderer = newRenderer(unitGraph.graph, state);
 
 
 const uiLegend = document.getElementById("legend") as HTMLElement;
@@ -194,43 +197,63 @@ for (let [pkg, color] of packageColors.entries()) {
     uiLegend.appendChild(div);
 }
 
-// Render nodes accordingly to the internal state:
-// If there is a hovered node, all non-neighbor nodes are greyed
-renderer.setSetting("nodeReducer", (node, data) => {
-  const res: Partial<NodeDisplayData> = { ...data };
+const uiLayers = document.getElementById("layers") as HTMLTextAreaElement;
 
-  if (state.hoveredNeighbors && !state.hoveredNeighbors.has(node) && state.hoveredNode !== node) {
-    res.label = "";
-    res.color = "#f6f6f6";
-  }else if(!!state.selectedPackage) {
-      const pkg = unitPackages.get(node)??'';
-      if (pkg != state.selectedPackage) {
-          res.label = "";
-          res.color = "#f6f6f6";
-      } else {
-          const color = packageColors.get(pkg) as string;
-          res.color = color;
-      }
-  }else {
-    const pkg = unitPackages.get(node)??'';
-    const color = packageColors.get(pkg) as string;
-    res.color = color;
-  }
+uiLayers.oninput = () => {
+    const layers = parseLayersConfig(uiLayers.value);
+    state.layersConfig = layers;
+    
+    const output = document.getElementById("output") as HTMLTextAreaElement;
+    output.value = "";
+    output.value += JSON.stringify(layers, null, 2);
+}
+
+interface Cluster {
+  label: string;
+  x?: number;
+  y?: number;
+  color?: string;
+  positions: { x: number; y: number }[];
+}
+
+// initialize clusters from graph data
+const countryClusters: { [key: string]: Cluster } = {};
 
 
-  return res;
+const uiApply = document.getElementById("apply") as HTMLButtonElement;
+uiApply.onclick = () => {
+    if (!state.layersConfig) {
+        return;
+    }
+    newRenderer(state.layersConfig.apply(unitGraph.graph), state);
+
+unitGraph.graph.forEachNode((node, atts) => {
+  const cluster = countryClusters[atts.country];
+  // node color depends on the cluster it belongs to
+  atts.color = cluster.color;
+  // node size depends on its degree
+  atts.size = Math.sqrt(unitGraph.graph.degree(node)) / 2;
+  // store cluster's nodes positions to calculate cluster label position
+  cluster.positions.push({ x: atts.x, y: atts.y });
 });
 
-// Render edges accordingly to the internal state:
-// 1. If a node is hovered, the edge is hidden if it is not connected to the
-//    node
-renderer.setSetting("edgeReducer", (edge, data) => {
-  const res: Partial<EdgeDisplayData> = { ...data };
+// calculate the cluster's nodes barycenter to use this as cluster label position
+for (const country in countryClusters) {
+  countryClusters[country].x =
+    countryClusters[country].positions.reduce((acc, p) => acc + p.x, 0) / countryClusters[country].positions.length;
+  countryClusters[country].y =
+    countryClusters[country].positions.reduce((acc, p) => acc + p.y, 0) / countryClusters[country].positions.length;
+}
 
-  if (state.hoveredNode && !graph.hasExtremity(edge, state.hoveredNode)) {
-    res.hidden = true;
-  }
-
-
-  return res;
-});
+// create the clustersLabel layer
+const clustersLayer = document.createElement("div");
+clustersLayer.id = "clustersLayer";
+let clusterLabelsDoms = "";
+for (const country in countryClusters) {
+  // for each cluster create a div label
+  const cluster = countryClusters[country];
+  // adapt the position to viewport coordinates
+  const viewportPos = renderer.graphToViewport(cluster as Coordinates);
+  clusterLabelsDoms += `<div id='${cluster.label}' class="clusterLabel" style="top:${viewportPos.y}px;left:${viewportPos.x}px;color:${cluster.color}">${cluster.label}</div>`;
+}
+}
---
+++ input.ts
@@ -0,0 +1,90 @@
+import Graph from "graphology";
+
+function loadUnits(): Graph {
+    const xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", "/units.txt", false );
+    xmlHttp.send( null );
+
+    const graph = new Graph();
+    for (const fqn of xmlHttp.responseText.split("\n").slice(1)) {
+        if ( 
+            /Test$/.test(fqn)
+            || /\\Test\\/.test(fqn)
+            || /SwagTest/.test(fqn)
+            || /Shopware.Core.Framework.Log.Package/.test(fqn)
+            || /Context$/.test(fqn)
+            || /Entity$/.test(fqn)
+            || /EntityRepository$/.test(fqn)
+            || /Definition$/.test(fqn)
+            || /Collection$/.test(fqn)
+            || /^Shopware\\Core\\DevOps\\/.test(fqn)
+            || /^Shopware\\Core\\Framework\\DataAbstractionLayer\\Search/.test(fqn)
+            || /Defaults$/.test(fqn)
+            || /ShopwareHttpException$/.test(fqn)
+            || /^Shopware\\Core\\Framework\\Struct/.test(fqn)
+            || /Uuid$/.test(fqn)
+            || /Feature$/.test(fqn)
+           ) {
+               continue;
+           }
+           const name = fqn.split("\\").pop();
+           graph.mergeNode(fqn, {name: name});
+    }
+    return graph;
+}
+
+
+function loadEdges(graph: Graph): void {
+    const xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", "/dependencies.csv", false );
+    xmlHttp.send( null );
+    const data = xmlHttp.responseText;
+
+    for (const line of data.split("\n")) {
+        const parts: string[] = line.split(",").map((p: string) => p.trim());
+        if (parts.length < 2) {
+            continue;
+        }
+
+        const self = parts[0];
+        if (!graph.hasNode(self)) {
+            continue;
+        }
+
+        if (parts[1] != "") {
+            graph.setNodeAttribute(self, "package", parts[1]);
+        }
+
+        for (const vertex of parts.slice(2)) {
+            if (!graph.hasNode(vertex) || vertex == self || vertex == "") {
+                continue;
+            }
+            graph.mergeEdge(self, vertex);
+        }
+    }
+    const loneNodes = graph.filterNodes((node: string) => graph.degree(node) == 0);
+    loneNodes.forEach((node: string) => graph.dropNode(node));
+}
+
+export interface UnitGraph {
+    graph: Graph;
+    getPackages(): Set<string>;
+}
+
+export function newUnitGraph(): UnitGraph {
+    const graph = loadUnits();
+    loadEdges(graph);
+    const packages: Set<string> = graph.nodes().reduce((acc: Set<string>, node: string) => {
+        const pkg = graph.getNodeAttribute(node, "package");
+        if (pkg) {
+            acc.add(pkg);
+        }
+        return acc;
+    }, new Set<string>());
+    return {
+        graph: graph,
+        getPackages: () => {
+            return packages;
+        }
+    };
+}
